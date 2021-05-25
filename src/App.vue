36667457ead8397e33dffb9207931d6655bbe6b2
oldskool123 <59705400+oldskool123@users.noreply.github.com>
2021-05-25 12:54:54 +0200

    Tracking user choice
    - Tracking users card choice
    - Validation if matches/ not matches

--- src/App.vue
+++ src/App.vue
@@ -8,14 +8,17 @@
       :value="card.value"
       :visible="card.visible"
       :position="card.position"
+      :matched="card.matched"
       @select-card="flipCard"
     />
   </section>
+
+  <h2>{{ status }}</h2>
 </template>
 
 <script>
 import Card from "./components/Card";
-import { ref } from "vue";
+import { ref, watch } from "vue";
 
 export default {
   name: "App",
@@ -23,23 +26,62 @@ export default {
     Card,
   },
   setup() {
+    // sets reactive
     const cardList = ref([]);
+    const userSelection = ref([]);
+    const status = ref("");
 
     for (let i = 0; i < 16; i++) {
       cardList.value.push({
         value: i,
         visible: false,
         position: i,
+        matched: false,
       });
     }
 
     const flipCard = (payload) => {
       cardList.value[payload.position].visible = true;
+
+      if (userSelection.value[0]) {
+        userSelection.value[1] = payload;
+      } else {
+        userSelection.value[0] = payload;
+      }
     };
+
+    // check if more than 2 cards selected
+    watch(
+      userSelection,
+      (currentValue) => {
+        if (currentValue.length === 2) {
+          const cardOne = currentValue[0];
+          const cardTwo = currentValue[1];
+
+          // Check if cards a match
+          if (cardOne.faceValue === cardTwo.faceValue) {
+            status.value = "Matched";
+
+            cardList.value[cardOne.position].matched = true;
+            cardList.value[cardTwo.position].matched = true;
+          } else {
+            status.value = "No match";
+
+            cardList.value[cardOne.position].visible = false;
+            cardList.value[cardTwo.position].visible = false;
+          }
+
+          userSelection.value.length = 0;
+        }
+      },
+      { deep: true } // track deep value
+    );
 
     return {
       cardList,
       flipCard,
+      userSelection,
+      status,
     };
   },
 };
