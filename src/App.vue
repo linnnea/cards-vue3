2ff0da6691b3b9720abf2868f9a26702b0432db1
oldskool123 <59705400+oldskool123@users.noreply.github.com>
2021-05-25 13:02:18 +0200

    Update App.vue
    - Check if player won condition
    - Watch for remaining cards

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,7 @@
 
 <script>
 import Card from "./components/Card";
-import { ref, watch } from "vue";
+import { computed, ref, watch } from "vue";
 
 export default {
   name: "App",
@@ -29,7 +29,22 @@ export default {
     // sets reactive
     const cardList = ref([]);
     const userSelection = ref([]);
-    const status = ref("");
+
+    const status = computed(() => {
+      if (remainingPairs.value === 0) {
+        return "You won!";
+      } else {
+        return `Remaining Pairs: ${remainingPairs.value}`;
+      }
+    });
+
+    const remainingPairs = computed(() => {
+      const remainingCards = cardList.value.filter(
+        (card) => card.matched === false
+      ).length;
+
+      return remainingCards / 2;
+    });
 
     for (let i = 0; i < 16; i++) {
       cardList.value.push({
